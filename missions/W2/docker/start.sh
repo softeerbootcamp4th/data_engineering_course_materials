525d21ce1fbc6b0732b638f5044c0379f13252c3
jang-namu <ironhee8005@gmail.com>
2024-07-11 12:27:49 +0900

    refactor: jupyter lab workdir /app 설정

--- missions/W2/docker/start.sh
+++ missions/W2/docker/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 깃허브 레포지토리 동기화
-if [ -d "/app/.git" ]; then
-    cd /app && git pull
+if [ -d "/app/softeer/.git" ]; then
+    cd /app/softeer && git pull
 else
-    git clone https://github.com/jang-namu/data-engineering-course.git /app
+    git clone https://github.com/jang-namu/data-engineering-course.git /app/softeer
 fi
 
 jupyter lab --ip='*' --port=8889 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
