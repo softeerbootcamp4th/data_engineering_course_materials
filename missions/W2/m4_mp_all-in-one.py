2dd288e8a69bb82f333df83589bee2172e5add7c
Neogr1 <mslee720@gmail.com>
2024-07-14 16:26:08 +0900

    refine week 2 mission 4 code

--- missions/W2/m4_mp_all-in-one.py
+++ missions/W2/m4_mp_all-in-one.py
@@ -7,36 +7,33 @@ from multiprocessing import Process, Queue, current_process
 from time import sleep
 import queue
 
-def f(tasks_to_accomplish, tasks_that_are_done):
-    try:
-        while True:
-            # task = tasks_to_accomplish.get() # 큐가 비지 않을 때까지 기다린 후 pop함 -> 종료 안 됨
-            task = tasks_to_accomplish.get_nowait()
+MAX_PROCESSES = 4
+
+def f(tasks_to_do, tasks_done):
+    while True:
+        try:
+            task = tasks_to_do.get_nowait()
             print(f"Task no {task}")
+            tasks_done.put((task, current_process().name))
+        except queue.Empty:
+            break
+        else:
             sleep(0.5)
-            tasks_that_are_done.put((task, current_process().name))
-    except queue.Empty:
-        pass
 
 if __name__ == '__main__':
-    tasks_to_accomplish = Queue()
-    tasks_that_are_done = Queue()
+    tasks_to_do = Queue()
+    tasks_done = Queue()
 
     for i in range(10):
-        tasks_to_accomplish.put(i)
+        tasks_to_do.put(i)
 
-    processes = [Process(
-        target=f,
-        args=(tasks_to_accomplish, tasks_that_are_done),
-        name=str(i) # 명시하지 않아도 자동으로 1부터 이름 매겨짐 
-        ) for i in range(1, 5)
-    ]
-
-    for proc in processes:
-        proc.start()
+    processes = []
+    for _ in range(MAX_PROCESSES):
+        processes.append(Process(target=f, args=(tasks_to_do, tasks_done)))
+        processes[-1].start()
     for proc in processes:
         proc.join()
 
-    while not tasks_that_are_done.empty():
-        task, proc = tasks_that_are_done.get()
+    while not tasks_done.empty():
+        task, proc = tasks_done.get()
         print(f"Task no {task} is done by Process-{proc}")
