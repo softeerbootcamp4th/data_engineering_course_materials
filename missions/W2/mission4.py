aa56103915b0e5b600fc2f7a289309493d756678
Gu Lee <99sphere@gmail.com>
2024-07-14 14:24:44 +0900

    fix: fix target function logic and change variable name.

--- missions/W2/mission4.py
+++ missions/W2/mission4.py
@@ -1,18 +1,19 @@
 import time
-from multiprocessing import Pool, Queue, Process, current_process, Manager, JoinableQueue
+from multiprocessing import Queue, Process, current_process
 
-NUM_TASK = 10
+NUM_TASKS = 10
 NUM_PROCESSES = 4
 
 def target_func(tasks_to_accomplish, tasks_that_are_done):
-    try:
-        while True:
+    while True:
+        try:
             val = tasks_to_accomplish.get_nowait()
+        except Exception as e: 
+            break   
+        else:
             print(f"Task no {val}")
             time.sleep(0.5)
             tasks_that_are_done.put(f"Task no {val} is done by {current_process().name}")
-    except Exception as e: 
-        pass          
     return
 
 if __name__=="__main__":
@@ -20,18 +21,18 @@ if __name__=="__main__":
     tasks_that_are_done = Queue()
     procs = []
     
-    for i in range(NUM_TASK):
+    for i in range(NUM_TASKS):
         tasks_to_accomplish.put(i)
     
     for i in range(NUM_PROCESSES):
         proc = Process(name=f'Process-{i+1}', target=target_func, args=(tasks_to_accomplish, tasks_that_are_done,))
-        procs.append(proc) 
+        procs.append(proc)
         procs[i].start()
-                
+        
     # Wait for all processes to finish.
     for proc in procs:
         proc.join()
         
-    for i in range(NUM_TASK):
+    for i in range(NUM_TASKS):
         msg = tasks_that_are_done.get()
         print(msg)
