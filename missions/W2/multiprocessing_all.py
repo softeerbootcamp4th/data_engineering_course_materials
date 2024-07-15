45a7d77afdb782824cd66c439f1e151e5d0ce927
YoonJunHyeok <jhyoon1607@gmail.com>
2024-07-15 15:15:59 +0900

    W2M4: while - try - except - else & one for loop

--- missions/W2/multiprocessing_all.py
+++ missions/W2/multiprocessing_all.py
@@ -1,39 +1,42 @@
 import time
+import queue
 from multiprocessing import Process, Queue, current_process
 
 def worker(tasks_to_accomplish: Queue, tasks_that_are_done: Queue):
-    try:
-        while True:
+    while True:
+        try:
             task = tasks_to_accomplish.get_nowait() # eqaul to 'get(False)'
-
+        except queue.Empty:
+            break
+        else:
             print(f"Task no {task}")
 
             time.sleep(0.5)
-            log = f"Task no {task} is done by Process-{current_process().name}"
 
+            log = f"Task no {task} is done by Process-{current_process().name}"
             tasks_that_are_done.put(log)
-    except Exception as e:
-        return
 
 if __name__ == "__main__":
     tasks_to_accomplish = Queue()
     tasks_that_are_done = Queue()
 
-    num_processes = 4
-
     # Add tasks to the queue
     for task in range(10):
         tasks_to_accomplish.put(task)
 
-    processes = [Process(
+    num_processes = 4
+
+    processes = []
+
+    for process_number in range(1, num_processes + 1):
+        process = Process(
             name=str(process_number), 
             target=worker, 
             args=(tasks_to_accomplish, tasks_that_are_done)
-        ) 
-        for process_number in range(1, num_processes + 1)
-    ]
+        )
 
-    for process in processes:
+        processes.append(process)
+        
         process.start()
 
     for process in processes:
