e56b23afcddce3514ad85daf60dfbd8c08a242bd
user name <gua05093@gmail.com>
2024-07-18 02:29:58 +0900

    W1M3 : rebuild ETL process

--- missions/W1/etl/etl_project_gdp_with_sql.py
+++ missions/W1/etl/etl_project_gdp_with_sql.py
@@ -7,247 +7,6 @@ from bs4 import BeautifulSoup
 
 
 """
-sqlite3 를 통해 sql 쿼리를 수행하여 조회한 내용을 반환하는 함수
-"""
-def execute_sql(sql_text) -> list[any]:
-    with sqlite3.connect('World_Economies.db') as conn:
-        cur = conn.cursor()
-        cur.execute(sql_text)
-        rows = cur.fetchall()
-        return rows
-            
-
-"""
-위키에서 국가별 GDP를 스크롤하여 리스트로 반환하는 함수
-[[국가, GDP], [국가, GDP], ...] 형태
-첫번째 행은 [모든 국가, GDP 총합]
-"""
-def scroll_wiki() -> list:
-    # 데이터 저장을 위한 리스트 초기화
-    table_data = [] 
-    url = "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29"
-    response = requests.get(url)
-    
-    # 요청이 성공했는지 확인
-    if response.status_code == 200:
-        
-        # HTML 파싱
-        soup = BeautifulSoup(response.text, 'html.parser')
-        table = soup.find_all('table')
-        tbody = table[2].find('tbody')
-        rows = tbody.find_all('tr')
-    
-        # 각 행의 데이터를 리스트 형태로 추출
-        for row in rows:
-            cells = row.find_all('td')[:2]
-            if not cells:
-                continue
-            cell_values = [cell.get_text(strip=True) for cell in cells]
-            if cell_values[1] == '\u2014':
-                cell_values[1] = '-1'
-            table_data.append(cell_values)
-            
-    # 요청 실패시 메시지 출력
-    else:
-        print("Failed to retrieve the web page")
-    
-    return table_data
-
-
-"""
-list 자료를 입력받아 json 파일로 내보내는 함수
-"""
-def list_to_json(_list) -> None:
-    df = pd.DataFrame(_list)
-    json_data = df.to_json(orient='records')
-    
-    with open('Countries_by_GDP.json', 'w') as f:
-        f.write(json_data)
-    
-
-"""
-json 파일을 열어 DB에 테이블을 생성하는 함수
-"""
-def open_json():
-    json_path = './Countries_by_GDP.json'
-    with open(json_path, 'r') as file:
-        data_list = json.load(file)
-    
-    with sqlite3.connect('World_Economies.db') as conn:
-        cur = conn.cursor()
-        cur.execute(
-                '''
-                CREATE TABLE IF NOT EXISTS Countries_by_GDP (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                Country TEXT UNIQUE,
-                GDP_USD_billion INT
-                )
-                '''
-        )
-        
-        for item in data_list:
-            if item['0'] == 'World':
-                continue
-            try:
-                cur.execute(
-                    '''
-                    INSERT INTO Countries_by_GDP (Country, GDP_USD_billion) VALUES (?, ?)
-                    ''',
-                    (item['0'], int(item['1'].replace(',','')))
-                )
-            except sqlite3.IntegrityError as e:
-                print(e)
-                break
-        conn.commit()
-
-
-"""
-국가,대륙 정보를 가지고 있는 region.txt 파일을 얼어
-국가,대륙 정보를 가지고 있는 테이블 CONTINENT를 생성한 후
-GDP, CONTINENT 테이블을 LEFT JOIN한 테이블 GDP_CONTI을 생성하는 함수
-"""
-def create_nation_conti_table():
-    txt_file = './region.txt'
-    nation_conti_list = []
-    with open(txt_file, mode='r', encoding='utf-8') as file:
-        for line in file:
-            line = line.strip()  # 줄 바꿈 문자 제거
-            if line:
-                nation, continent = line.split(',')
-                nation_conti_list.append((nation, continent))
-    
-    with sqlite3.connect('World_Economies.db') as conn:
-        cur = conn.cursor()
-        cur.execute(
-                '''
-                CREATE TABLE IF NOT EXISTS CONTINENT (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                nation TEXT UNIQUE,
-                continent TEXT
-                )
-                '''
-        )
-        
-        for item in nation_conti_list:
-            try:
-                cur.execute(
-                    '''
-                    INSERT INTO CONTINENT (nation, continent) VALUES (?, ?)
-                    ''',
-                    (item[0], item[1])
-                )
-            except sqlite3.IntegrityError as e:
-                print(e)
-                break
-            
-        cur.execute(
-                """
-                CREATE TABLE IF NOT EXISTS NATION_CONTI  (
-                nation TEXT PRIMARY KEY,
-                gdp INTEGER,
-                continent TEXT
-                );
-                """
-        )
-        
-        conn.commit()
-        try:
-            cur.execute(
-                    """
-                    INSERT INTO NATION_CONTI (nation, gdp, continent)
-                    SELECT
-                        Countries_by_GDP.Country,
-                        Countries_by_GDP.GDP_USD_billion,
-                        CONTINENT.continent
-                    FROM
-                        Countries_by_GDP
-                    LEFT JOIN
-                        CONTINENT ON Countries_by_GDP.Country = CONTINENT.nation;
-                    """
-            )
-        except:
-            print()
-            
-        conn.commit()
-        
-        
-"""
-데이터프레임으로서 load한 내용을 요구사항에 맞게 정리하여 출력하는 함수
-"""
-def analyze():
-    nation_gdp_list = execute_sql(
-    """
-    SELECT *
-    FROM Countries_by_GDP
-    WHERE GDP_USD_billion >= 100000
-    """
-    )
-
-    region_average_list = execute_sql(
-    """
-    WITH RankedGDP AS (
-        SELECT
-            nation,
-            continent,
-            gdp,
-            ROW_NUMBER() OVER (PARTITION BY continent ORDER BY gdp DESC) AS row_num
-        FROM
-            NATION_CONTI
-    )
-    SELECT
-        continent,
-        AVG(gdp) AS average_top_5_gdp
-    FROM
-        RankedGDP
-    WHERE
-        row_num <= 5
-    GROUP BY
-        continent;
-    """
-    )
-    
-    print('\n[Nations with GDP exceeding 100B USD (Unit:Billion $)]')
-    for item in nation_gdp_list:
-        
-        float_gdp = item[2] / 1000
-        print(f'{item[0]:<4}{item[1]:<25}{float_gdp:<12.2f}')
-        
-    print('\n[Top 5 GDP averages in each region (Unit:Billion $)]')
-    for item in region_average_list:
-        if not item[0]:
-            continue
-        float_gdp = item[1] / 1000
-        print(f'{item[0]:15} : {float_gdp:.2f}')
-        
-
-"""
-국가,대륙 정보가 담긴 파일을 이용하여 딕셔너리 자료를 구성
-중복되지 않는 대륙 정보를 사용하여 딕셔너리 자료를 구성
-{키 = 대륙 : 값 = GDP 값 리스트} 
-"""
-def trans_region_data() -> tuple[dict, dict]:
-    # 텍스트 파일 경로
-    txt_file = 'region.txt'
-
-    # 빈 dictionary 생성
-    nation_continent_dict = dict()
-    continent_set = set()
-
-    # 텍스트 파일 읽기
-    with open(txt_file, mode='r', encoding='utf-8') as file:
-        for line in file:
-            line = line.strip()  # 줄 바꿈 문자 제거
-            if line:
-                nation, continent = line.split(',')
-                nation_continent_dict[nation] = continent
-                continent_set.add(continent)
-                
-    continent_GDP_dict = {item : [] for item in continent_set}
-
-    return nation_continent_dict, continent_GDP_dict
-    
-
-"""
 datatime library를 통해 현재 시각을 적절한 포맷의 문자열로 반환하는 함수
 """    
 def get_cur_time():
@@ -269,39 +28,262 @@ def log(process : str) -> None:
     with open(filename, 'a+') as file:
         # 파일에 쓸 콘텐츠 추가
         file.write(log_string + '\n')
+        
+        
+"""
+sqlite3 를 통해 sql 쿼리를 수행하여 조회한 내용을 반환하는 함수
+"""
+def execute_sql(sql_text) -> list[any]:
+    with sqlite3.connect('World_Economies.db') as conn:
+        cur = conn.cursor()
+        cur.execute(sql_text)
+        rows = cur.fetchall()
+        return rows
+            
+
+"""
+위키에서 국가별 GDP를 스크롤하여 데이터프레임으로 반환하는 함수
+"""
+def scroll_wiki() -> pd.DataFrame:
+    url = "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29"
+    response = requests.get(url)
+    
+    # 요청이 성공했는지 확인
+    if response.status_code == 200:
+        
+        # HTML 파싱
+        soup = BeautifulSoup(response.text, 'html.parser')
+        table = soup.find_all('table')
+        tbody = table[2].find('tbody')
+        rows = tbody.find_all('tr')
+        
+        # 행의 개수를 파악하여 빈 데이터프레임 생성
+        num_rows = len(rows)
+        df = pd.DataFrame(index=range(num_rows), columns=['Country', 'GDP (million)'])
+
+        # 각 행의 데이터를 데이터프레임에 추가
+        row_index = 0
+        for row in rows:
+            cells = row.find_all('td')[:2]
+            if not cells:
+                continue
+            cell_values = [cell.get_text(strip=True) for cell in cells]
+            if cell_values[1] == '\u2014':
+                cell_values[1] = '-1'
+            # 데이터프레임에 행 추가
+            df.loc[row_index] = cell_values
+            row_index += 1
+        
+        # 실제 데이터가 들어간 부분만 추출
+        df = df.iloc[:row_index]
+        
+        return df
+    else:
+        # 요청이 실패한 경우 빈 데이터프레임 반환
+        return pd.DataFrame()
+
+
+"""
+데이터프레임을 json 파일로 내보내는 함수
+"""
+def df_to_json(df : pd.DataFrame) -> None:
+    json_data = df.to_json(orient='records')
+    
+    with open('Countries_by_GDP.json', 'w') as f:
+        f.write(json_data)    
+
+
+"""
+json 파일을 열어 데이터프레임으로 반환하는 함수
+"""
+def json_to_df(json_path: str) -> pd.DataFrame:
+    df = pd.read_json(json_path)
+    return df
+
+
+"""
+DB에서 대륙 정보를 가져오는 함수 (다른 절차를 통해 이미 만들어진 데이터)
+"""
+def get_continent_data(db_path: str) -> pd.DataFrame:
+    # SQLite 데이터베이스에 연결
+    conn = sqlite3.connect(db_path)
+    
+    # SQL 쿼리를 사용하여 데이터 가져오기
+    query = "SELECT * FROM CONTINENT"
+    df = pd.read_sql_query(query, conn)
+    
+    # 데이터베이스 연결 종료
+    conn.close()
+    
+    if df.empty:
+        log('No data found in the CONTINENT table.')
+    
+    return df
+
+
+"""
+트랜스폼 함수
+필요한 데이터가 있는지 확인(이전 프로세스, 준비된 데이터)
+필요한 데이터가 있으면 transform 수행
+"""
+def transform() -> pd.DataFrame:
+    db_path = 'World_Economies.db'
+    df_continent_world = get_continent_data(db_path=db_path)    
+    df_continent_world.columns = ['id','Country', 'Continent']
+    
+    json_path = './Countries_by_GDP.json'
+    df_countries_by_GDP = json_to_df(json_path)
+    df_countries_by_GDP.columns = ['Country', 'GDP_USD_million']
+    
+    if df_continent_world.empty or df_countries_by_GDP.empty:
+        log('No data found in the CONTINENT or Countries_by_GDP table.')
+        return
+    
+    # df_merged.column : Country, GDP_USD_million, Continent
+    df_merged = pd.merge(df_countries_by_GDP, df_continent_world[['Country', 'Continent']], on='Country', how='left')
+    
+    # GDP_USD_million을 쉼표 제거 후 숫자로 변환하고 GDP_USD_billion으로 변환
+    df_merged['GDP_USD_billion'] = df_merged['GDP_USD_million'].apply(
+        lambda x: (pd.to_numeric(x.replace(',', ''), errors='coerce') / 1000).round(2) if pd.notna(x) else None
+    )
+    
+    # 필요 없는 컬럼 삭제
+    df_merged = df_merged.drop(columns=['GDP_USD_million'])
+    
+    # 컬럼 순서 재배치
+    df_merged = df_merged[['Country', 'GDP_USD_billion', 'Continent']]
+    return df_merged
+
+    
+"""
+데이터베이스에 데이터를 적재하는 함수
+"""
+def load_to_db(df: pd.DataFrame):
+    db_path = 'World_Economies.db'
+    
+    # SQLite 데이터베이스에 연결 및 데이터 삽입
+    with sqlite3.connect(db_path) as conn:
+        # 데이터프레임을 'NATION_CONTI' 테이블에 삽입
+        df.to_sql('NATION_CONTI', conn, if_exists='replace', index=False)
+        
+        # Check if the table exists in the database
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='NATION_CONTI'")
+        table_exists = cursor.fetchone()
+        
+        if table_exists:
+            log("NATION_CONTI table exists in the database.")
+        else:
+            log("NATION_CONTI table does not exist in the database.")
+    
+    # 로그 메시지 출력
+    log('Data inserted into NATION_CONTI table.')
     
 
-################################
-## wiki -- scraping --> jason ##
-################################
+"""
+데이터베이스에 SQL 쿼리를 실행하고 결과를 DataFrame으로 반환하는 함수
+"""
+def query_database(query: str, db_path: str) -> pd.DataFrame:
+    """Execute a SQL query and return the results as a DataFrame."""
+    with sqlite3.connect(db_path) as conn:
+        return pd.read_sql_query(query, conn)        
+   
+        
+"""
+데이터베이스에서 GDP가 1000B 이상인 국가를 조회하는 함수
+"""
+def analyze_gdp_exceeding_1000B():
+    db_path = 'World_Economies.db'
+    
+    # Query for nations with GDP exceeding 1000 billion USD
+    nation_gdp_query = """
+    SELECT Country, GDP_USD_billion
+    FROM NATION_CONTI
+    WHERE GDP_USD_billion >= 1000
+    """
+    nation_gdp_df = query_database(nation_gdp_query, db_path)
+    nation_gdp_df = nation_gdp_df.iloc[1:]
+    
+    # Print nations with GDP exceeding 1000 billion USD
+    print('\n[Nations with GDP exceeding 1000B USD (Unit: Billion $)]')
+    print(nation_gdp_df)
+    
+    
+"""
+데이터베이스에서 대륙별 상위 5개 국가의 GDP 평균을 계산하는 함수
+"""
+def analyze_top_5_GDP_average():
+    db_path = 'World_Economies.db'
+    
+    # Query for the top 5 GDP averages in each continent
+    region_average_query = """
+    WITH RankedGDP AS (
+        SELECT
+            Country,
+            GDP_USD_billion,
+            Continent,
+            ROW_NUMBER() OVER (PARTITION BY Continent ORDER BY GDP_USD_billion DESC) AS row_num
+        FROM
+            NATION_CONTI
+    )
+    SELECT
+        Continent,
+        AVG(GDP_USD_billion) AS average_top_5_gdp
+    FROM
+        RankedGDP
+    WHERE
+        row_num <= 5
+    GROUP BY
+        Continent;
+    """
+    
+    region_average_df = query_database(region_average_query, db_path)
+    region_average_df = region_average_df.iloc[1:]
+        
+    # Print top 5 GDP averages in each region
+    print('\n[Top 5 GDP averages in each region (Unit: Billion $)]')
+    print(region_average_df)
+    
+
+###############################
+## wiki -- scraping --> json ##
+###############################
 
 #E : start extract
 log('E : start extract')
-gdp_list = scroll_wiki()
+scrolled_dataframe = scroll_wiki()
+df_to_json(scrolled_dataframe)
 
 #E : end extract
 log('E : end extract')
 
-#T : start transform (list -> json)
-log('T : start transform (list -> json)')
-list_to_json(gdp_list)
 
-#T : end transform (list -> json)
+#########################################
+## input data    : scrolled data(json) ##
+## reserved data : CONTINENT(DB TABLE) ##
+#########################################
+
+#T : start transform 
+log('T : start transform')
+df_transformed = transform()
+
+#T : end transform 
 log('T : end transform (list -> json)')
 
-####################################
-## json -- open, put data into db ##
-####################################
+
+################################
+## load transfored data to DB ##
+################################
  
 #L : start load
 log('L : start load')
-create_nation_conti_table()
-open_json()
+load_to_db(df_transformed)
 
-# #L : end load
+#L : end load
 log('L : end load')
 
 #######################################################
 ## db -- analyze by using sql query and print result ##
 #######################################################
-analyze()
+analyze_gdp_exceeding_1000B()
+analyze_top_5_GDP_average()
