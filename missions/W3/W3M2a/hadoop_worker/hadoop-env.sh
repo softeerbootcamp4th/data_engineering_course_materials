025bb211f93451d5db2d11efe82d4ed93de82662
ys <90746505+yongzza@users.noreply.github.com>
2024-07-21 19:13:13 +0900

    Update files and add README.md

--- missions/W3/W3M2a/hadoop_worker/hadoop-env.sh
+++ missions/W3/W3M2a/hadoop_worker/hadoop-env.sh
@@ -8,11 +8,11 @@ export HADOOP_LOG_DIR=$HADOOP_HOME/logs
 export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
 
 # Define HDFS and YARN user
-export HDFS_NAMENODE_USER=root
-export HDFS_DATANODE_USER=root
-export HDFS_SECONDARYNAMENODE_USER=root
-export YARN_RESOURCEMANAGER_USER=root
-export YARN_NODEMANAGER_USER=root
+export HDFS_NAMENODE_USER=hdfs
+export HDFS_DATANODE_USER=hdfs
+export HDFS_SECONDARYNAMENODE_USER=hdfs
+export YARN_RESOURCEMANAGER_USER=yarn
+export YARN_NODEMANAGER_USER=yarn
 
 # YARN logs
 export HADOOP_LOG_DIR=/usr/local/hadoop/logs
