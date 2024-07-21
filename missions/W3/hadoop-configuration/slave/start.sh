64a48d448ad75539cd1e9e0a6f0fb9490093d854
jang-namu <ironhee8005@gmail.com>
2024-07-22 02:18:40 +0900

    feat: skeleton of modifying

--- missions/W3/hadoop-configuration/slave/start.sh
+++ missions/W3/hadoop-configuration/slave/start.sh
@@ -25,8 +25,8 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export HADOOP_VERSION=3.4.0
 
 # docker volume 부착 시, root:root (user:group)으로 부착되어서 dfs 디렉토리 내에 name, data 등 디렉토리를 생성하지 못하는 문제 해결
-chown -R hdfs:hadoop /usr/local/hadoop/data
-chmod -R 770 /usr/local/hadoop/data
+chown -R hdfs:hadoop $HADOOP_HOME/data
+chmod -R 770 $HADOOP_HOME/data
 echo "Starting SSH service"
 service ssh start
 
@@ -40,13 +40,13 @@ su -c "ssh -o StrictHostKeyChecking=no localhost 'exit'" hdfs
 su -c "ssh -o StrictHostKeyChecking=no localhost 'exit'" yarn
 
 echo "Starting datanode as hdfs user"
-su -c "export PDSH_RCMD_TYPE=ssh && export HADOOP_HOME=/usr/local/hadoop && export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop && export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin && export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 && export HADOOP_VERSION=3.4.0 && hadoop-daemon.sh start datanode" hdfs
+sudo -E -u hdfs $HADOOP_HOME/sbin/hadoop-daemon.sh start datanode
 
 echo "Setting permissions for Hadoop logs"
 chmod -R 770 $HADOOP_HOME/logs
 
 echo "Starting nodemanager daemons as yarn user"
-su -c "export PDSH_RCMD_TYPE=ssh && export HADOOP_HOME=/usr/local/hadoop && export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop && export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin && export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 && export HADOOP_VERSION=3.4.0 && yarn-daemon.sh start nodemanager" yarn
+sudo -E -u yarn $HADOOP_HOME/sbin/yarn-daemon.sh start nodemanager
 
 # 포그라운드 프로세스를 유지하기 위한 임시 명령 (예시)
 echo "Keeping a foreground process to maintain the script running"
