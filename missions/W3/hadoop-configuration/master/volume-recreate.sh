76c234d18df4a87ee6e6d343b7c7d7dcfe20130d
jang-namu <ironhee8005@gmail.com>
2024-07-25 10:49:18 +0900

    chore: volume rm create script

--- missions/W3/hadoop-configuration/master/volume-recreate.sh
+++ missions/W3/hadoop-configuration/master/volume-recreate.sh
@@ -3,4 +3,4 @@
 docker volume rm hadoop-master-data hadoop-slave1-data hadoop-slave2-data
 docker volume create hadoop-master-data
 docker volume create hadoop-slave1-data
-docker volume create hadoop-slave2-data
+docker volume create hadoop-slave2-dagta
