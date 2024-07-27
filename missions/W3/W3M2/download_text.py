1ff856906faa5b31e2c74884cd8f1f552b3d4395
admin <admin@admins-MacBook-Pro-3.local>
2024-07-28 03:18:35 +0900

    [DE한경훈] Feat: 한경훈 W3M2 (Dockerfile, docker-compose, configs, scripts,,,) files submit

---
+++ missions/W3/W3M2/download_text.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL to fetch
+url = 'https://www.gutenberg.org/cache/epub/74139/pg74139-images.html'
+
+# Path to save the text file
+save_path = '/Users/admin/Desktop/Data_Engineering/W3/W3M2a/Multi_Node_Hadoop_M2/mapreduce_example.txt'
+
+# Fetch the URL content
+response = requests.get(url)
+response.raise_for_status()
+
+# Parse the HTML content
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Extract text from the HTML
+text = soup.get_text()
+
+# Save the extracted text to a file
+with open(save_path, 'w', encoding='utf-8') as file:
+    file.write(text)
+
+print(f'Text successfully saved to {save_path}')
