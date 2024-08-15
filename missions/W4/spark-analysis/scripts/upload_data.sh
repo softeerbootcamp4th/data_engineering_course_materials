0458d90d6671fa2c1ef92c0dc5b0deb194999f81
jang-namu <ironhee8005@gmail.com>
2024-08-16 02:15:57 +0900

    chore: 데이터 업로드 스크립트 수정

--- missions/W4/spark-analysis/scripts/upload_data.sh
+++ missions/W4/spark-analysis/scripts/upload_data.sh
@@ -2,5 +2,5 @@
 
 hdfs dfs -mkdir -p /user/root/data
 for file in /home/hdfs/examples/*; do
-  sudo -u hdfs $HADOOP_HOME/bin/hdfs dfs -put $file /user/root/data
+  sudo -u hdfs $HADOOP_HOME/bin/hdfs dfs -put $file /user/root/data/
 done
